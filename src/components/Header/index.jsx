7f84b8bb279938482f3fdb6c094e45d84f5b95d9
unknown <anna06491@gmail.com>
2024-09-29 12:50:30 +0200

    Add html and css of  header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,16 @@
 import './index.css';
 
 export const Header = () =>{
-    return(<></>)
+    return(<>
+        <div className="header__container">
+            <div className="header__contant">
+                <p className="header__logo">LM</p>
+                <div className="header__menu">
+                    <a href="" className="menu__item">Топ</a>
+                    <a href="" className="menu__item">Подобный фильм</a>
+                    <a href="" className="menu__item">Случайный фильм</a>
+                </div>
+            </div>
+        </div>
+    </>)
 }
